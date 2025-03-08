7c4c5fa45870c575ee00ae1298bdbe97f06f6373
Redandan <coconut0989@gmail.com>
2025-03-08 22:34:03 +0800

    自動部署: 2025-03-08 22:34:03

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "441272513"
+    serviceWorkerVersion: "1241449893"
   }
 });
