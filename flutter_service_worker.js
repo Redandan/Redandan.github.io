42009f9ba5e2f38c130227fcaf2e432ecf5eeede
Redandan <coconut0989@gmail.com>
2025-08-07 00:06:36 +0800

    Auto deploy: 2025-08-07 00:06:36 - Version 1.0.124

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -56,7 +56,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d71158da1c0b7cfc181c2f13eaaa95db
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "c768dd16793f19ea3c68197a5a01f7c4",
+"flutter_bootstrap.js": "337440cb5f3a1bf6c3f246ac3272526f",
 "icons/agora-market-icon.svg": "0809310c9c8ce8213a71b912864eb06c",
 "icons/android-icon-144x144.png": "63fa5dc585dccfddad1550ea018e4ce3",
 "icons/android-icon-192x192.png": "a5813fca5d400b7712cd910d9a868f31",
