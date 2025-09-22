53cf92844caad575c85780fe1505fbf7a5a0544c
Redandan <coconut0989@gmail.com>
2025-09-22 19:19:52 +0800

    Auto deploy: 2025-09-22 19:19:51 - Version 1.0.225

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3767109720"
+    serviceWorkerVersion: "4195141125"
   }
 });
