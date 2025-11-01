4349028a9da654371d98cd24607fe0b0dce45fa6
Redandan <coconut0989@gmail.com>
2025-11-01 18:16:08 +0800

    Auto deploy: 2025-11-01 18:16:08 - Version 1.0.429

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "331928004"
+    serviceWorkerVersion: "3731454954"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -60,7 +60,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d71158da1c0b7cfc181c2f13eaaa95db
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "260984a3ae7d858593a3fb0cd23f9eec",
+"flutter_bootstrap.js": "a7c5afa269a38f70fde0a50b8c011986",
 "icons/agora-market-icon.svg": "0809310c9c8ce8213a71b912864eb06c",
 "icons/android-icon-144x144.png": "63fa5dc585dccfddad1550ea018e4ce3",
 "icons/android-icon-192x192.png": "a5813fca5d400b7712cd910d9a868f31",
@@ -93,11 +93,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "d71158da1c0b7cfc181c2f13eaaa95db
 "icons/ms-icon-150x150.png": "9223022b000ee835a7c091cefd0197a2",
 "icons/ms-icon-310x310.png": "0d56d3ab620dd4e6ace7b00bf4aaa962",
 "icons/ms-icon-70x70.png": "d950bf1443b00a5c8391e8ffc7ff3eeb",
-"index.html": "dc056a1c773e3d6228e7224bb653fb72",
-"/": "dc056a1c773e3d6228e7224bb653fb72",
-"main.dart.js": "7f856039403a2ac892ec15cf3399e388",
+"index.html": "6f1a14b00000f9d8fc005cc55273663c",
+"/": "6f1a14b00000f9d8fc005cc55273663c",
+"main.dart.js": "07925d1f8d21df5e19bfe62490f5fe90",
 "main.dart.mjs": "55fe6f6bdb3315e28a04b73b2fabee78",
-"main.dart.wasm": "c86c450d7d4d137fd0b3f166d2698d88",
+"main.dart.wasm": "f1d4f9de86eb44defdd044526ad515e8",
 "manifest.json": "c918cd892f1180ef22cf86743676f02f",
 "official.html": "8d9f1ea643c08f7c8e25b200fa52e173",
 "register.html": "2d1fd710acdbde1488faad3f465589b1",
@@ -105,7 +105,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d71158da1c0b7cfc181c2f13eaaa95db
 "splash/iPhone_14_portrait.png": "96e752610906ba2a93c65f8abe1645f1",
 "splash/iPhone_14_Pro_Max_portrait.png": "96e752610906ba2a93c65f8abe1645f1",
 "splash/iPhone_14_Pro_portrait.png": "96e752610906ba2a93c65f8abe1645f1",
-"version.json": "9c9d295e3d35ed007f86e24c5fe5ba0e"};
+"version.json": "3db039941ecc3197b6e8cba819442e31"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
