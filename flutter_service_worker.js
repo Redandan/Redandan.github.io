876345da603ba763a18d4dd89f345bd33daaa665
Redandan <coconut0989@gmail.com>
2025-11-20 12:07:57 +0800

    Auto deploy: 2025-11-20 12:07:57 - Version 1.0.505

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -64,7 +64,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "409c83c8ea3222382671240279559b9b
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "841afd3cbd7bffc3186c9c604d4981c0",
+"flutter_bootstrap.js": "635d86db41734bb68fc65afd8fb7e789",
 "icons/agora-market-icon.svg": "0809310c9c8ce8213a71b912864eb06c",
 "icons/android-icon-144x144.png": "63fa5dc585dccfddad1550ea018e4ce3",
 "icons/android-icon-192x192.png": "a5813fca5d400b7712cd910d9a868f31",
@@ -97,9 +97,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "409c83c8ea3222382671240279559b9b
 "icons/ms-icon-150x150.png": "9223022b000ee835a7c091cefd0197a2",
 "icons/ms-icon-310x310.png": "0d56d3ab620dd4e6ace7b00bf4aaa962",
 "icons/ms-icon-70x70.png": "d950bf1443b00a5c8391e8ffc7ff3eeb",
-"index.html": "0cf4bc75d93483d299de342d2ec0a026",
-"/": "0cf4bc75d93483d299de342d2ec0a026",
-"main.dart.js": "8055786a558510a96565f6df3f310e93",
+"index.html": "03847ca56ac6b5ed990a9ce826127676",
+"/": "03847ca56ac6b5ed990a9ce826127676",
+"main.dart.js": "dbf7a474c6adb957a4945ee8831ac7a7",
 "manifest.json": "295fdc01af0bb2a5299af31d35118ea3",
 "official.html": "8d9f1ea643c08f7c8e25b200fa52e173",
 "offline.html": "b087704827a6d51f6dc2d77948b1dd5c",
@@ -110,7 +110,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "409c83c8ea3222382671240279559b9b
 "splash/iPhone_14_portrait.png": "96e752610906ba2a93c65f8abe1645f1",
 "splash/iPhone_14_Pro_Max_portrait.png": "96e752610906ba2a93c65f8abe1645f1",
 "splash/iPhone_14_Pro_portrait.png": "96e752610906ba2a93c65f8abe1645f1",
-"version.json": "fb37dc503c2b46a2dd6a770b353ac0b0",
+"version.json": "920bd15b5f61e89c60fb3c643f9a2ae2",
 "web_vitals.js": "7c0c4e6144e1544cdd6cf55db9a4f6c4"};
 // The application shell files that are downloaded before a service worker can
 // start.
