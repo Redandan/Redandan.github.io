01b612fd4d74cd53aadd1fb1af21514bd72c1fdf
Redandan <coconut0989@gmail.com>
2025-12-04 15:42:43 +0800

    Auto deploy TRON Wallet Adapter (Vite): 2025-12-04 15:42:43

---
+++ wellect-connect/assets/all-wallets.CtgZNv0t.js
@@ -0,0 +1,6 @@
+import{b as l}from"./wallet-vendor.BYI0E9G4.js";import"./react-vendor.CY-G_Z4Y.js";import"./buffer-polyfill.D4-N1JiB.js";const i=l`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{i as allWalletsSvg};
