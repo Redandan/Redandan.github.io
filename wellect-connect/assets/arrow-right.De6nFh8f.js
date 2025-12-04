d3d93a54931abbbf6a956b94a43df78b302e5193
Redandan <coconut0989@gmail.com>
2025-12-04 15:55:42 +0800

    Auto deploy TRON Wallet Adapter (Vite): 2025-12-04 15:55:42

---
+++ wellect-connect/assets/arrow-right.De6nFh8f.js
@@ -0,0 +1,8 @@
+import{b as l}from"./wallet-vendor.CHv2NQr2.js";import"./react-vendor.Bj0G0Oyz.js";import"./buffer-polyfill.ErO7y5re.js";const i=l`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M1 7.99a1 1 0 0 1 1-1h7.58L7.12 4.53A1 1 0 1 1 8.54 3.1l4.16 4.17a1 1 0 0 1 0 1.41l-4.16 4.17a1 1 0 1 1-1.42-1.41l2.46-2.46H2a1 1 0 0 1-1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{i as arrowRightSvg};
--- wellect-connect/assets/cursor.poVSk6O7.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./wallet-vendor.BYI0E9G4.js";import"./react-vendor.CY-G_Z4Y.js";import"./buffer-polyfill.D4-N1JiB.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
