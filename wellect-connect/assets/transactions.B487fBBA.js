b88c4a518d621f10ca28bf593ee804f3d288b070
Redandan <coconut0989@gmail.com>
2025-12-04 15:22:16 +0800

    Auto deploy TRON Wallet Adapter (Vite): 2025-12-04 15:22:16

---
+++ wellect-connect/assets/transactions.B487fBBA.js
@@ -0,0 +1,16 @@
+import{i as f,a as m,x as a,c as p}from"./wallet-vendor.bkLHWOah.js";import"./if-defined.B6mSN0f9.js";import"./index.D7P52xuJ.js";import"./react-vendor.rHj4_3Hj.js";import"./index.8tTQQ-Wq.js";import"./index.CcC3V829.js";import"./index.zQN6bqtY.js";import"./index.DDXZV29H.js";import"./index.BXZc6km6.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var c=o.length-1;c>=0;c--)(l=o[c])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let s=class extends m{render(){return a`
+      <wui-flex flexDirection="column" .padding=${["0","m","m","m"]} gap="s">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=d;s=u([p("w3m-transactions-view")],s);export{s as W3mTransactionsView};
