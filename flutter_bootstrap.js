2f4b2353a43a609efbaea5e121aeb56b8f1ed4f7
Redandan <coconut0989@gmail.com>
2026-03-03 16:30:30 +0800

    Auto deploy: 2026-03-03 16:30:30 - Version 1.0.792

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1447629224" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4094301597" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
