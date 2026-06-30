47bb49a3e88c3b00d7e694ac2d83006e3bc85a1a
Redandan <coconut0989@gmail.com>
2026-06-30 12:21:44 +0800

    Auto deploy: 2026-06-30 12:21:44 - Version 1.0.1814

---
+++ main.dart.1.0.1814.js_54.part.js
@@ -0,0 +1,6 @@
+((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
+A=c[0]
+B=c[94]
+var z=a.updateTypes([]);(function constants(){B.GW=new A.br("\u67e5\u770b\u8a73\u60c5",null,null,null,null,null,null,null,null,null,null)})()};
+(a=>{a["55+qV/tr1UpQBZGqTUrxDi6O0nU="]=a.current})($__dart_deferred_initializers__);
