0be20e10342cfc393a777f64e7ff9c95f55bcfd7
Redandan <coconut0989@gmail.com>
2026-09-02 17:11:50 +0800

    Remove retired Flutter Web test assets (#28)
    * Remove retired Flutter Web test assets
    
    * Keep Pages redirect best effort
    
    * Normalize direct Pages fallback entry

--- main.dart.1.0.1917.js_76.part.js
+++
@@ -1,7 +0,0 @@
-((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B={
-e4O(){return new A.xQ(null)}}
-A=c[37]
-B=a.updateHolder(c[19],B)
-var z=a.updateTypes([])};
-(a=>{a["BRPR+CyfTOxlx7KvLyIlUGzfDQM="]=a.current})($__dart_deferred_initializers__);
